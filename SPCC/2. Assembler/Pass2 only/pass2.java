37786bbc88d2ea33b379f3f57645a85f4db47b2d
Shardul Mahadik <shardulhere@gmail.com>
2014-05-05 02:04:28 +0530

    Complete

--- SPCC/2. Assembler/Pass2 only/pass2.java
+++ SPCC/2. Assembler/Pass2 only/pass2.java
@@ -13,6 +13,8 @@ class pass1{
 		literal_table = new ArrayList<Literal>();
 		fillMOT();
 		fillPOT();
+		fillSymbolTable();
+		fillLiteralTable();
 		File input_file = new File("twopass_input.txt");
 		Scanner in = new Scanner(input_file);
 		File output_file = new File("twopass_output.txt");
@@ -20,15 +22,11 @@ class pass1{
 		String input = in.nextLine();
 		String s[] = split(input);
 		int pc = (s[2]!=null)?Integer.parseInt(s[2]):0;
-		symbol_table.add(new Symbol(s[0],0,1,"R"));
 		out.write(pc+"\n");
+		int base = 0;
 		while(in.hasNextLine()){
 			input = in.nextLine();
 			s = split(input);
-			if(s[0]!=null&&s[0].length()>0){
-				if(!inSymbolTable(s[0]))
-					symbol_table.add(new Symbol(s[0],pc,4,"R"));
-			}
 			if(s[1].equals("DC")){
 				int length = 0;
 				if(s[2].charAt(0)=='B') length=1;
@@ -36,15 +34,8 @@ class pass1{
 				if(s[2].charAt(0)=='F') length=4;
 				if(s[2].charAt(0)=='D') length=8;
 				while(pc%length!=0) pc++;
-				if(!inSymbolTable(s[0])){
-					symbol_table.add(new Symbol(s[0],pc,length,"R"));
-				}else{
-					Symbol sym = findSymbol(s[0]);
-					sym.value = pc;
-					sym.length = length;
-				}
-				out.write(pc+" "+s[2].substring(2,s[2].length()-1)+"\n");
-				pc = pc+length;
+				out.write(pc+" "+Integer.toBinaryString(Integer.parseInt(s[2].substring(2,s[2].length()-1)))+"\n");
+				pc += length;
 			}
 			if(s[1].equals("DS")){
 				int length = 0;
@@ -53,36 +44,24 @@ class pass1{
 				if(s[2].charAt(s[2].length()-1)=='F') length=4;
 				if(s[2].charAt(s[2].length()-1)=='D') length=8;
 				while(pc%length!=0) pc++;
-				if(!inSymbolTable(s[0])){
-					symbol_table.add(new Symbol(s[0],pc,length,"R"));
-				}else{
-					Symbol sym = findSymbol(s[0]);
-					sym.value = pc;
-					sym.length = length;
-				}
 				out.write(pc+"\n");
 				pc = pc+length*Integer.parseInt(s[2].substring(1,s[2].length()-2));
 			}
 			if(s[1].equals("EQU")){
-				int val = (s[2].trim().equals("*"))?pc:Integer.parseInt(s[2]);
-				if(!inSymbolTable(s[0])){
-					symbol_table.add(new Symbol(s[0],val,1,"A"));
-				}else{
-					Symbol sym = findSymbol(s[0]);
-					sym.value = val;
-					sym.length = 1;
-					sym.ra = "A";
-				}
 				out.write(pc+"\n");
 			}
-			if(s[1].equals("USING")||s[1].equals("DROP")){
+			if(s[1].equals("USING")){
+				base = Integer.parseInt(s[3]);
+				out.write(pc+"\n");
+			}
+			if(s[1].equals("DROP")){
+				base = 0;
 				out.write(pc+"\n");
 			}
 			if(s[1].equals("END")){
 				for(Literal l : literal_table){
-					l.value = pc;
-					out.write(pc+" "+l.name.substring(3,l.name.length()-1)+"\n");
-					pc += 4;
+					out.write(pc+" "+Integer.toBinaryString(Integer.parseInt(l.name.substring(3,l.name.length()-1)))+"\n");
+					pc += l.length;
 				}
 				out.write(pc+"\n");
 			}
@@ -90,40 +69,18 @@ class pass1{
 				MOT m = findMOT(s[1]);
 				out.write(pc+" "+s[1]+" ");
 				if(isInteger(s[2])) out.write(s[2]);
-				else out.write("_");
+				else out.write(findSymbol(s[2]).value+"");
 				out.write(",");
 				if(isInteger(s[3])) out.write(s[3]);
-				else out.write("_");
+				else if(s[3].charAt(0)=='=') out.write(findLiteral(s[3]).value+"");
+				else out.write(findSymbol(s[3]).value+"");
+				if(m.rr.equals("RX")) out.write("(0,"+base+")");
 				out.write("\n");
 				pc += m.length;
-			}
-			if(s[3]!=null&&s[3].length()>0){
-				if(s[3].charAt(0)=='=')
-					if(!inLiteralTable(s[3]))
-						literal_table.add(new Literal(s[3],0,4,"R"));
 			}
 		}
 		out.flush();
 		out.close();
-		File symbol_file = new File("symbol.txt");
-		BufferedWriter symbol_out = new BufferedWriter(new FileWriter(symbol_file));
-		for(Symbol sym : symbol_table)
-			symbol_out.write(sym.name+"\t"+sym.value+"\t"+sym.length+"\t"+sym.ra+"\n");
-		symbol_out.flush();
-		symbol_out.close();
-		File literal_file = new File("literal.txt");
-		BufferedWriter literal_out = new BufferedWriter(new FileWriter(literal_file));
-		for(Literal l : literal_table)
-			literal_out.write(l.name+"\t"+l.value+"\t"+l.length+"\t"+l.ra+"\n");
-		literal_out.flush();
-		literal_out.close();
-		
-	}
-	static boolean inLiteralTable(String str){
-		for(Literal l : literal_table)
-			if(l.name.equals(str))
-				return true;
-		return false;
 	}
 	static boolean inMOT(String str){
 		for(MOT m:mot)
@@ -137,16 +94,16 @@ class pass1{
 				return m;
 		return null;
 	}
-	static boolean inSymbolTable(String str){
-		for(Symbol s:symbol_table)
-			if(s.name.equals(str))
-				return true;
-		return false;
-	}
 	static Symbol findSymbol(String str){
 		for(Symbol s:symbol_table)
 			if(s.name.equals(str))
 				return s;
+		return null;
+	}
+	static Literal findLiteral(String str){
+		for(Literal l : literal_table)
+			if(l.name.equals(str))
+				return l;
 		return null;
 	}
 	static String[] split(String s){
@@ -160,6 +117,22 @@ class pass1{
 				toReturn[k++] = s.substring(start,i+1).trim();
 		}
 		return toReturn;
+	}
+	static void fillSymbolTable() throws Exception{
+		File f = new File("symbol.txt");
+		Scanner sc = new Scanner(f);
+		while(sc.hasNextLine()){
+			String s[] = sc.nextLine().split("\t");
+			symbol_table.add(new Symbol(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),s[3]));
+		}
+	}
+	static void fillLiteralTable() throws Exception{
+		File f = new File("literal.txt");
+		Scanner sc = new Scanner(f);
+		while(sc.hasNextLine()){
+			String s[] = sc.nextLine().split("\t");
+			literal_table.add(new Literal(s[0],Integer.parseInt(s[1]),Integer.parseInt(s[2]),s[3]));
+		}
 	}
 	static void fillMOT() throws Exception{
 		File f = new File("mot.txt");
